f6398b88bdae8e84d1b44055bb2511cd1f81374c
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2025-07-20 05:56:59 +0000

    Move wheel to root and update script

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./install.sh && uvicorn app.main:app --host 0.0.0.0 --port $PORT
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "=== Instalando modelo wheel ==="
+pip install ./model_abandono-0.0.1-py3-none-any.whl
+
+echo "=== Instalando dependencias ==="
+pip install uvicorn>=0.20.0,<0.30.0
+pip install fastapi>=0.88.0,<1.0.0
+pip install python-multipart>=0.0.5,<0.1.0
+pip install typing_extensions>=4.2.0,<5.0.0
+pip install loguru>=0.5.3,<1.0.0
+pip install pydantic>=1.10.0,<2.0.0
